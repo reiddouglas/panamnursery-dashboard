4bb7eb0da2e7ef17100d4f5ffe3d0cd31db65873
Reid W <reiddwilliamson@gmail.com>
2025-11-25 04:31:08 -0800

    add base api url to env

--- src/app/features/auth/services/user-api-service.ts
+++ src/app/features/auth/services/user-api-service.ts
@@ -3,14 +3,14 @@ import { Injectable, inject } from '@angular/core';
 import { UserCreateDTO } from '../models/user-create.dto';
 import { Observable } from 'rxjs';
 import { UserLoginDTO } from '../models/user-login.dto';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserApiService {
-  private baseUrl = 'https://panamapi-abgpgphwafbvezdj.canadacentral-01.azurewebsites.net/api/user';
-
   http = inject(HttpClient);
+  baseUrl = `${environment.apiUrl}/api/user/`;
 
   createUser(user: UserCreateDTO): Observable<UserCreateDTO> {
     return this.http.post<UserCreateDTO>(this.baseUrl, user);
